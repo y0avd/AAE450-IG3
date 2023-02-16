fc771c600d47416e15c9a2cbee9d84c68784fccf
Yoav Dekel <83027923+y0avd@users.noreply.github.com>
2023-02-16 12:34:12 -0500

    Fixed bug

--- get_dvGA.m
+++ get_dvGA.m
@@ -1,12 +1,10 @@
-function [dvGA, delta] = get_dvGA(vp,vi,flybyr,planetID)
+function [dvGA, rp] = get_dvGA(v_rel,delta,planetID)
     mu = getPlanetMu(planetID);
 
-    v_rel_in = vi - vp;
-    v_inf = norm(v_rel_in);
+    v_inf = norm(v_rel);
     specE = v_inf^2/2;
     a = -mu/(2*specE);
-    e = 1 - flybyr/a;
-    delta = 2*asin(1/e); %rads
+    rp = a*(1-1/sin(delta/2));
 
     dvGA = sqrt(2*v_inf^2 - 2*v_inf^2*cos(delta));
 end
